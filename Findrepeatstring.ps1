dd5004fbaa4f13a43f2b726767d120dd8426444b
Imranscript <75296397+Imranscript@users.noreply.github.com>
2020-12-01 00:19:41 -0600

    Add files via upload

---
+++ Findrepeatstring.ps1
@@ -0,0 +1,19 @@
+﻿#Example
+#findrepeat [string]
+#findrepeat findtherepeatcharacter
+function findrepeat{
+ 
+ Param([parameter(Mandatory=$true)][String]$string)
+ 
+ $chars = @()
+ #Turns string into an array
+ $string.ToCharArray()  | %{
+                            $chars += @($_)
+                           }
+#finds non repeated characters
+$nonrepeatchar = $chars | select –unique
+
+#compares non repeated characters with repeated characters, organizes it in an array, and then selects the first repeated object (character)
+(Compare-object –referenceobject $nonrepeatchar –differenceobject $chars)[0].inputobject
+
+}
